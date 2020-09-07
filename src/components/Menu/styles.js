b8f233e659622eacb9297ed624ee304e3e644218
Allan Wanderley <awallan259@gmail.com>
2020-09-07 00:51:04 -0300

    Feat: Changed drawer image

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -13,7 +13,7 @@ import { Link } from 'react-router-dom';
 
 import { devices } from '@/config/devices';
 
-import LogoSmall from '@/assets/logo-unicarioca-vertical.png';
+import LogoSmall from '@/assets/favicon.png';
 
 const drawerWidth = 80;
 
