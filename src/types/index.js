68ed48951e53ca8d74df12b5d334c381a8cff130
Allan Wanerley Alves <awallan259@gmail.com>
2020-11-01 11:02:53 -0300

    Mudança de proptypes para refletir o novos models

--- src/types/index.js
+++ src/types/index.js
@@ -9,148 +9,32 @@ import {
 } from 'prop-types';
 
 export const userType = shape({
-  publicProfile: bool,
-  platformStats: bool,
-  firstLogin: bool,
-  id: number,
-  name: string,
+  id: string,
+  nome: string,
   email: string,
-  gender: string,
-  tagAuthor: string,
-  tagAdmin: string,
-  cellphone: string,
-  birthDate: oneOfType([
-    Date,
-    string,
-    object,
-  ]),
-  address: string,
-  number: oneOfType([
-    number,
-    string,
-  ]),
-  password: string,
-  deleted: bool,
-  customUrl: string,
-  created_at: oneOfType([
+  genero: string,
+  url: string,
+  dataCadastro: oneOfType([
     Date,
     string,
   ]),
-  updatedAt: oneOfType([
-    Date,
-    string,
-  ]),
-  confirmEmail: string,
-  confirmEmailToken: string,
-  lastEmailTokenSendAt: number,
-  profilePhoto: string,
 });
 
 export const themeType = shape({
   id: number,
-  name: string,
-  alias: string,
-  description: string,
-  state: string,
+  nome: string,
+  descricao: string,
 });
 
 export const categoryType = shape({
   id: number,
-  name: string,
-  alias: string,
-  description: string,
-  theme: themeType,
-  state: oneOf([
-    'active',
-    'removed',
-  ]),
+  nome: string,
+  descricao: string,
+  tema: themeType,
 });
 
 export const articleType = shape({
   id: number,
-});
-
-export const ticketNotificationType = shape({
-  id: number,
-  content: shape({
-    readed: bool,
-    type: string,
-    userId: number,
-    email: string,
-    msg: string,
-    createdAt: oneOfType([
-      Date,
-      string,
-    ]),
-    updatedAt: oneOfType([
-      Date,
-      string,
-    ]),
-  }),
-});
-
-export const ticketType = shape({
-  id: number,
-});
-
-export const commentType = shape({
-  id: number,
-  userName: string,
-  userEmail: string,
-  message: string,
-  article: articleType,
-  confirmedAt: oneOfType([
-    Date,
-    string,
-  ]),
-  readedAt: oneOfType([
-    Date,
-    string,
-  ]),
-  createdAt: oneOfType([
-    Date,
-    string,
-  ]),
-  updatedAt: oneOfType([
-    Date,
-    string,
-  ]),
-});
-
-export const commentSettingsType = shape({
-  userId: number,
-  type: oneOf([
-    'all',
-    'not-readed',
-    'only-readed',
-  ]),
-  order: oneOf([
-    'desc',
-    'asc',
-  ]),
-  limit: number,
-  notify: bool,
-  answersOrder: oneOf([
-    'desc',
-    'asc',
-  ]),
-  answersType: oneOf([
-    'all',
-    'enabled',
-    'disabled',
-  ]),
-});
-
-export const statType = shape({
-  id: number,
-  month: number,
-  count: number,
-  generated_at: oneOfType([
-    Date,
-    string,
-  ]),
-  year: number,
-  reference: string,
 });
 
 export const appTheme = oneOf([
