2fc0c7039b42dac040e959661a745587f2488378
Allan Wanerley Alves <awallan259@gmail.com>
2020-11-15 14:57:02 -0300

    Corrigido redirecionamento de rotas quando é acesso um recurso que não existe na aplicação

--- src/components/NotFound/RouteNotFound.jsx
+++ src/components/NotFound/RouteNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 function RouteNotFound() {
-  return <Redirect to="/" />;
+  return <Redirect to="/auth" />;
 }
 
 export default RouteNotFound;
